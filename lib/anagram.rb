c24c0323d693e6fdb9a83fe9dea4765677f5552f
Francis <roccofrancis@gmail.com>
2016-07-27 17:43:59 -0400

    Complete Lab: Anagrams

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array_of_words)
+    anagrams = []
+    array_of_words.each do |potential_anagram|
+      anagrams << potential_anagram if potential_anagram.chars.sort == @word.chars.sort
+    end
+    anagrams
+  end
+
+end
